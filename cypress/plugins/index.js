d5124b2c3c15c726a63211820d64383b877a9518
Gary Wong <Gary.T.Wong@gov.bc.ca>
2020-05-12 21:43:21 -0700

    Interim save with Direct Access Grant

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -14,4 +14,15 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  console.log(config) // see what all is in here!
+
+  // modify config values
+  config.defaultCommandTimeout = 10000
+  config.baseUrl = 'https://dev-invasivesbc.pathfinder.gov.bc.ca'
+
+  // modify env var value
+  config.env.ENVIRONMENT = 'dev'
+
+  // return config
+  return config
 }
